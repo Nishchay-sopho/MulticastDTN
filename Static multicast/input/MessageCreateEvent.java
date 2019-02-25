3f6ba31f56b7e46eefc452605f002e7c7415bfc4
Nishchay-sopho <agrawal.nishchay5@gmail.com>
2019-02-25 10:26:22 +0530

    Implementation of multicast which is static

---
+++ Static multicast/input/MessageCreateEvent.java
@@ -0,0 +1,93 @@
+/* 
+ * Copyright 2010 Aalto University, ComNet
+ * Released under GPLv3. See LICENSE.txt for details. 
+ */
+package input;
+
+import core.DTNHost;
+import core.Message;
+import core.World;
+
+import java.util.ArrayList;
+
+/**
+ * External event for creating a message.
+ */
+public class MessageCreateEvent extends MessageEvent {
+	private int size;
+	private int responseSize;
+	private String msgContent;
+
+	/** Added for multicast and broadcast implementation on 12-02-2019 **/
+	private boolean multicast;
+	private Integer[] destination_list;
+	
+	/**
+	 * Creates a message creation event with a optional response request
+	 * @param from The creator of the message
+	 * @param to Where the message is destined to
+	 * @param id ID of the message
+	 * @param size Size of the message
+	 * @param responseSize Size of the requested response message or 0 if
+	 * no response is requested
+	 * @param time Time, when the message is created
+	 */
+	public MessageCreateEvent(int from, int to, String id, int size,
+			int responseSize, double time,String msgContent,boolean multicast,Integer[] destination_list) {
+		super(from,to, id, time);
+		//System.out.println("Initiating constructor for messageCreateEvent");
+		this.size = size;
+		this.responseSize = responseSize;
+		this.msgContent=msgContent;
+		this.multicast=multicast;
+		this.destination_list=destination_list;
+		//System.out.println("Finished constructor for messageCreateEvent");
+	}
+
+	public MessageCreateEvent(int from, int to, String id, int size,
+							  int responseSize, double time,String msgContent) {
+		super(from,to, id, time);
+		this.size = size;
+		this.responseSize = responseSize;
+		this.msgContent=msgContent;
+	}
+
+	public MessageCreateEvent(int from, int to, String id, int size,
+							  int responseSize, double time) {
+		super(from,to, id, time);
+		this.size = size;
+		this.responseSize = responseSize;
+	}
+	
+	/**
+	 * Creates the message this event represents. 
+	 */
+	@Override
+	public void processEvent(World world) {
+		DTNHost to = world.getNodeByAddress(this.toAddr);
+		DTNHost from = world.getNodeByAddress(this.fromAddr);
+
+		if(multicast){
+			ArrayList<DTNHost> multicastDestinations=new ArrayList<DTNHost>();
+			for(int i=0;i<destination_list.length;i++){
+				multicastDestinations.add(world.getNodeByAddress(this.destination_list[i]));
+			}
+			Message m= new Message(from,to,this.id,this.size,multicastDestinations);
+			m.setResponseSize(this.responseSize);
+			m.setContent(this.msgContent);
+			from.createNewMessage(m);
+		}
+		else {
+			Message m = new Message(from, to, this.id, this.size);
+			m.setResponseSize(this.responseSize);
+			m.setContent(this.msgContent);
+			from.createNewMessage(m);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + " [" + fromAddr + "->" + toAddr + "] " +
+		"size:" + size + " CREATE";
+	}
+}
---
+++ Static multicast/input/MulticastEventGenerator.java
@@ -0,0 +1,105 @@
+//Created by Nishchay on 12/02/2019
+
+package input;
+
+import java.util.*;
+
+import core.Settings;
+import core.SettingsError;
+
+public class MulticastEventGenerator extends MessageEventGenerator{
+    protected int[] destinationRange;
+
+    public static final String MULTICAST_DEST_RANGE = "destinationRange";
+
+    public static final String MULTICAST="multicast";
+    public static final String MULTICAST_NR_OF_DESTINATION="nrofDestination";
+
+    public static final String MULTICAST_DEST="multicastDest";
+    //public static final String MULTICAST_DEST_NUM="multicastDestinationsNum";
+
+    private ArrayList<Integer> toIds;
+    private int destinations;
+
+    private Settings s;
+
+    private boolean multicast;
+    private int[] multicastIDs;
+
+    public MulticastEventGenerator(Settings s){
+        super(s);
+        this.destinationRange=s.getCsvInts(MULTICAST_DEST_RANGE,2);
+        this.multicast=s.getBoolean(MULTICAST);
+        this.destinations=s.getInt(MULTICAST_NR_OF_DESTINATION);
+        this.toIds=new ArrayList<Integer>();
+        this.multicastIDs=s.getCsvInts(MULTICAST_DEST);
+//        if (toHostRange==null){
+//            throw new SettingsError("Destination host ("+TO_HOST_RANGE_S+") must be defined");
+//        }
+        for(int i=destinationRange[0];i<destinationRange[1];i++){
+            toIds.add(i);
+        }
+//        if((destinationRange[1]-destinationRange[0])!=destinations){
+//            throw new SettingsError("Destination address range ("+MULTICAST_DEST_RANGE+") and number of destination ("+MULTICAST_NR_OF_DESTINATION+ ") must be identical");
+//        }
+       // destination_list=new int[]{destinationRange[0],destinationRange[1]};
+        Collections.shuffle(toIds,rng);
+    }
+
+    /** Method picking up the destinations of the multicast in the Settings file and checking if the id lies in the destinationRange and then returning to the user.**/
+    private Integer[] multicast_destination_list(){
+      //  int[] multicastIDs=s.getCsvInts(MULTICAST_DEST,2);
+        int numOfMulticastDestinations=multicastIDs.length;
+        List<Integer> multicastTempID= convertToList(multicastIDs);
+        for(int i=0;i<numOfMulticastDestinations;i++){
+            if (!(multicastTempID.get(i) > destinationRange[0] && multicastTempID.get(i)<destinationRange[1])) {
+                multicastTempID.remove(multicastTempID.get(i));
+            }
+        }
+        return multicastTempID.toArray(new Integer[multicastTempID.size()]);
+
+    }
+    /** Read from the settings file the destinations entered by the user**/
+//    public Integer drawDestAddress(){
+//        return s.getCsvInts(MULTICAST_DEST_NUM);
+//    }
+
+    public ExternalEvent nextEvent(){
+        int responseSize = 0; /* zero stands for one way messages */
+        int msgSize;
+        int interval;
+        int from;
+        int to;
+        Integer[] to_multicast;
+
+        /* Get two *different* nodes randomly from the host ranges */
+        from = drawHostAddress(this.hostRange);
+        to = drawToAddress(hostRange, from);
+
+        msgSize = drawMessageSize();
+        interval = drawNextEventTimeDiff();
+
+        to_multicast=multicast_destination_list();
+
+        /* Create event and advance to next event */
+        MessageCreateEvent mce = new MessageCreateEvent(from, to, this.getID(),
+                msgSize, responseSize, this.nextEventsTime,"test",this.multicast,to_multicast);
+        this.nextEventsTime += interval;
+
+        if (this.msgTime != null && this.nextEventsTime > this.msgTime[1]) {
+            /* next event would be later than the end time */
+            this.nextEventsTime = Double.MAX_VALUE;
+        }
+
+        return mce;
+    }
+
+    private List<Integer> convertToList(int[] arr){
+        List<Integer> intList = new ArrayList<Integer>();
+        for (int i : arr)
+        {
+            intList.add(i);
+        }
+        return intList;
+    }
+}
